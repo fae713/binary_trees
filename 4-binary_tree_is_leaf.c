fbd19b944f138dbc2c3a25748dc8698ad7326b4b
K.A. Osei-Sarfo <astrokojo@gmail.com>
2024-01-04 23:40:27 +0000

    done 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -5,17 +5,15 @@
  *
  * @node: the node to check
  *
- * Return: 1 (Success)) 0 (Failure)
+ * Return: 1 (Success) 0 (Failure)
  */
-int binary_tree_is_leaf(binary_tree_t *node)
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
 
 	if (node->left == NULL && node->right == NULL)
-		return (1)
-
+		return (1);
 	else
 		return (0);
-
 }
