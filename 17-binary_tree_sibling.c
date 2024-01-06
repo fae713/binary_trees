befcf8e18954043815d67701ddcc6cda69327653
K.A. Osei-Sarfo <astrokojo@gmail.com>
2024-01-06 00:05:24 +0000

    fixed 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,17 +9,15 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *parent;
-
-	parent = node->parent;
-
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
-	if (parent->left == node && parent->right != NULL)
-		return (parent->right);
-	else if (parent->right == node && parent->left != NULL)
-		return (parent->left);
+	if (node->parent->left == node && node->parent->right != NULL)
+		return (node->parent->right);
+
+	else if (node->parent->right == node && node->parent->left != NULL)
+		return (node->parent->left);
+
 	else
 		return (NULL);
 }
